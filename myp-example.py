5f501c147ffb9ce4fab649b92e38e9dbb58a4b41
vineeth2101 <viswanathanvineeth01@gmail.com>
2023-10-20 18:22:59 +0530

    mypy test commit

---
+++ myp-example.py
@@ -0,0 +1,19 @@
+# example.py
+
+import os
+import sys
+import math
+
+
+def add_numbers(a: int, b: int) -> int:
+    result = a + b
+    return result
+
+
+def main() -> None:
+    print(add_numbers(5, 10))
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
